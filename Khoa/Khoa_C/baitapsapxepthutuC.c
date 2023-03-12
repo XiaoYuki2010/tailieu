701b159444163763fdaa53ea03830b5ac8634ba8
KhoaDKC <changtraicodon992001@gmail.com>
2023-03-13 03:05:55 +0700

    sync
    sync

---
+++ Khoa/Khoa_C/baitapsapxepthutuC.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[6]={67,3,9,2,34,5};
+    int b;
+    int c;
+    int temd;
+    int e;
+
+        for(b=0;b<6;b++)
+            { 
+                printf("bien so B[%d]: %d\n",b,a[b]);
+            }
+
+
+    
+}
